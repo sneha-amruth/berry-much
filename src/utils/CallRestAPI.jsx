3e4ee2bc0a02923889b695d81f2c14133344f1d7
sneha.amruth <sneha.amruth@servicenow.com>
2021-05-12 14:18:49 +0530

    bug: api connection

--- src/utils/CallRestAPI.jsx
+++ src/utils/CallRestAPI.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
+const { REACT_APP_API_URL } = process.env;
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: REACT_APP_API_URL,
     headers: { "content-type": "application/json" }
 })
 
