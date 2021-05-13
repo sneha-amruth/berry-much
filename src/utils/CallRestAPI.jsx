cff84197cdddc912108a986a0ec2580c310580ba
sneha.amruth <sneha.amruth@servicenow.com>
2021-05-13 11:49:17 +0530

    logging error

--- src/utils/CallRestAPI.jsx
+++ src/utils/CallRestAPI.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 const { REACT_APP_API_URL } = process.env;
 
+console.log("REACT_APP_API_URL,"+REACT_APP_API_URL);
 export const api = axios.create({
     baseURL: REACT_APP_API_URL,
     headers: { "content-type": "application/json" }
